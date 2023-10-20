dac147367ef51ba1977f7788ebfeae9f6bf86529
Abdollah Jalilian <stat4aj@gmail.com>
2023-10-20 11:18:30 +0100

    spatial covars

--- Ethiopia/codes/eth_map_covariates.R
+++ Ethiopia/codes/eth_map_covariates.R
@@ -8,132 +8,108 @@ library("terra")
 eth_map <- 
   readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/ETH_Admin_2021_OCHA.rds"))
 
-# =========================================================
-# function to read and extract raster data
-extract_raster_data <- function(downloaded_file,
-           data_file,
-           temp_dir="/tmp/eth_raster/")
-{
-    # create a temporary directory to extract the downloaded file
-    dir.create(temp_dir)
-    # extract the downloaded file
-    unzip(zipfile=downloaded_file, exdir=temp_dir)
-    
-    # read the raster data using the 'terra' package
-    rast(paste0(temp_dir, data_file))
-}
+# merge Woreds in each zone
+eth_map <- eth_map %>%
+  group_by(ADM2_EN) %>%
+  summarise(geometry=st_union(geometry)) %>%
+  ungroup()
 
 # =========================================================
+# spatial covariates
+# =========================================================
+# function to read and extract raster data
+extract_raster_covars <- function(downloaded_file,
+                                  data_file,
+                                  map,
+                                  stat="mean",
+                                  temp_dir="/tmp/eth_raster/")
+{
+  # create a temporary directory to extract the downloaded file
+  if (!dir.exists(temp_dir))
+  {
+    dir.create(temp_dir)
+  }
+  # extract the downloaded file
+  unzip(zipfile=downloaded_file, exdir=temp_dir)
+  
+  # read the raster data using the 'terra' package
+  raster <- rast(paste0(temp_dir, data_file))
+  
+  out <- vector("list", length=nrow(map))
+  for (i in 1:nrow(map))
+  {
+    cp <- terra::crop(raster, map[i, ], mask=TRUE, touches=TRUE)
+    cp <- values(cp)
+    cp <- cp[!is.na(cp)]
+    
+    out[[i]] <- switch(stat, mean={
+      c(mean=mean(cp), sd=sd(cp))
+    }, mode={
+      cp <- sort(table(cp), decreasing=TRUE)
+      cp <- round(100 * cp[1] / sum(cp), 2)
+      c(mode=names(cp), percent=unname(cp))
+    })
+  }
+  
+  # clean up the temporary directory
+  unlink(temp_dir, recursive=TRUE)
+  
+  return(bind_rows(out))
+}
+
 # read the Gridded Population of the World (GPW), v4
+# resolution: 30 arc second
 # provided by Socioeconomic Data and Applications Center, NASA
 # https://sedac.ciesin.columbia.edu/data/collection/gpw-v4
 pop_density <- 
-  extract_raster_data(
-    "~/Downloads/gpw-v4-population-density-rev11_2020_30_sec_tif.zip",
-    "gpw_v4_population_density_rev11_2020_30_sec.tif"
+  extract_raster_covars(
+    paste0("~/Downloads/Africa_covars/",
+           "gpw-v4-population-density-rev11_2020_30_sec_tif.zip"),
+    "gpw_v4_population_density_rev11_2020_30_sec.tif",
+    map=eth_map, stat="mean"
   )
 
 # read S2 Prototype Land Cover 20m Map of Africa 2016
+# resolution: 20 meter
 # provided by CCI Land Cover (LC) team, European Space Agency
 # https://2016africalandcover20m.esrin.esa.int/
-africa_land_cover <- 
-  extract_raster_data(
-    "~/Downloads/ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.zip",
-    "ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.tif"
+land_cover <- 
+  extract_raster_covars(
+    paste0("~/Downloads/Africa_covars/",
+    "ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.zip"),
+    "ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.tif",
+    map=eth_map, stat="mode"
   )
 
 # Void-filled digital elevation mode of Africa, 2007 [ArcGRID]
 # resolution: 15s resolution
 # provided by World Wildlife Fund. Africa, U.S. Geological Survey
 # https://maps.princeton.edu/catalog/stanford-jm998sr5227 
-africa_elevation <- 
-  extract_raster_data(
-    "~/Downloads/data.zip",
-    "af_dem_15s/"
+elevation <- 
+  extract_raster_covars(
+    paste0("~/Downloads/Africa_covars/",
+    "data.zip"),
+    "af_dem_15s/",
+    map=eth_map, stat="mean"
   )
 
-# =========================================================
-# plot population density and land cover for the whole country
-library("tidyterra")
-ggplot() +
-  geom_spatraster(data=mask(crop(pop_density, eth_map),
-                            eth_map),
-                  aes(fill=gpw_v4_population_density_rev11_2020_30_sec))
-ggplot() +
-  geom_spatraster(data=mask(crop(africa_land_cover, eth_map),
-                            eth_map),
-                  aes(fill=`ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0`))
+# combine raster covariates
+spat_covars <-
+  bind_cols(
+    ADM2_EN=eth_map %>% pull(ADM2_EN), 
+    pop_density %>% 
+      setNames(paste0('pop_density_', names(.))),
+    land_cover %>% 
+      setNames(paste0('land_cover_', names(.))),
+    elevation %>% 
+      setNames(paste0('elevation_', names(.)))
+  )
+
+# save the extracted spatial covariates
+saveRDS(spat_covars, file="spat_covars.rds")
 
 # =========================================================
-# extract population density, land cover, and elevation data for map units
-
-# merge Woreds in each zone
-map <- 
-  eth_map %>%
-  group_by(ADM2_EN) %>%
-  summarise(geometry = sf::st_union(geometry)) %>%
-  ungroup()
-
-rm(pop_density, africa_land_cover, africa_elevation)
-
-# extract covariates
-covar_fun <- function(map)
-{
-  
-  raster_1 <- terra::crop(pop_density, 
-                          map %>% 
-                            summarise(geometry=st_union(geometry)),
-                          mask=TRUE, touches=TRUE)
-  raster_2 <- terra::crop(africa_land_cover, 
-                          map %>% 
-                            summarise(geometry=st_union(geometry)), 
-                          mask=TRUE, touches=TRUE)
-  raster_3 <- terra::crop(africa_elevation, 
-                          map %>% 
-                            summarise(geometry=st_union(geometry)), 
-                          mask=TRUE, touches=TRUE)
-  
-  out <- vector("list", length=nrow(map))
-  for (i in 1:nrow(map))
-  {
-    cp1 <- terra::crop(raster_1, map[i, ], mask=TRUE, touches=TRUE)
-    cp1 <- values(cp1)
-    cp1 <- cp1[!is.na(cp1)]
-    
-    cp2 <- terra::crop(raster_2, map[i, ], mask=TRUE, touches=TRUE)
-    cp2 <- values(cp2)
-    cp2 <- cp2[!is.na(cp2)]
-    cp2 <- sort(table(cp2), decreasing=TRUE)
-    cp2 <- round(100 * cp2[1] / sum(cp2), 2)
-    
-    cp3 <- terra::crop(raster_3, map[i, ], mask=TRUE, touches=TRUE)
-    cp3 <- values(cp3)
-    cp3 <- cp3[!is.na(cp3)]
-    
-    gc()
-    
-    out[[i]] <- 
-      c(popdens_mean=mean(cp1), 
-        popdens_sd=sd(cp1), 
-        landcov_mode=as.integer(names(cp2)),
-        landcov_percent=unname(cp2),
-        elevation_mean=mean(cp3), 
-        elevation_sd=sd(cp3))
-  }
-  
-  return(out)
-}
-
-
-covars <- covar_fun(map)
-
-covars <- 
-  bind_cols(ADM2_EN=
-              unique(eth_map %>% 
-                       pull(ADM2_EN)), 
-            bind_rows(covars))
-
-
+# spatiotemporal covariates
 # =========================================================
 library("ecmwfr")
 library("ncdf4")
