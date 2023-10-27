0a178ce46f1bd60693dae38efa68fd4b7bef447d
Abdollah Jalilian <stat4aj@gmail.com>
2023-10-27 12:55:38 +0100

    Update eth_data_analyses.R

--- Ethiopia/codes/eth_data_analyses.R
+++ Ethiopia/codes/eth_data_analyses.R
@@ -8,4 +8,21 @@ library("tidyverse")
 eth_data <- 
   readRDS("~/Downloads/Ethiopia/eth_data.rds")
 
-eth_data %>% count(Date)
+# remove all records of week 53 of the year 2015
+eth_data <- eth_data %>%
+  filter(!is.na(Date))
+
+
+eth_data %>%
+  full_join(
+    data.frame(Date=seq(as.Date("2013-01-07"), 
+                        as.Date("2022-08-15"), 
+                        by=7)),
+    by = join_by(Date)
+  )
+
+
+eth_data %>% 
+  count(Date) %>%
+  ggplot(aes(x=Date, y=n)) +
+  geom_line()
