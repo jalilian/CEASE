61733bfdc0ba48b3097fe5daf425b32c678715db
Abdollah Jalilian <stat4aj@gmail.com>
2023-11-03 13:00:29 +0000

    modelling

--- Ethiopia/codes/eth_data_analyses.R
+++ Ethiopia/codes/eth_data_analyses.R
@@ -12,6 +12,12 @@ eth_data <-
 eth_data <- eth_data %>%
   filter(!is.na(Date))
 
+# data from a shapefile provided by OCHA
+eth_map <- 
+  readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/ETH_Admin_2021_OCHA.rds"))
+
+# =========================================================
+# missing data patterns
 
 # number of zones with recorded cases by date
 data.frame(Date=seq(as.Date("2013-01-07"), 
@@ -22,11 +28,8 @@ data.frame(Date=seq(as.Date("2013-01-07"),
   ggplot(aes(x=Date, y=(1 - n / 92))) +
   geom_line() +
   labs(y="percentage of missing weekly malaria records by date") +
-  scale_y_continuous(labels=scales::percent)
-
-# data from a shapefile provided by OCHA
-eth_map <- 
-  readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/ETH_Admin_2021_OCHA.rds"))
+  scale_y_continuous(labels=scales::percent)  +
+  theme_light()
 
 
 # number of weeks with recorded cases by zones
@@ -39,5 +42,39 @@ eth_map %>%
             by=join_by(ZoneName)) %>%
   ggplot(aes(fill=1 - n/374)) + 
   geom_sf() +
-  scale_fill_continuous(name="missing", 
-                        labels=scales::percent)
+  scale_fill_distiller(name="missing", 
+                       labels=scales::percent,
+                       palette = "Reds", direction=1)+
+  theme_light()
+
+# =========================================================
+# confirmed cases 
+
+# temporal pattern
+data.frame(Date=seq(as.Date("2013-01-07"), 
+                    as.Date("2022-08-15"), 
+                    by=7)) %>%
+  left_join(eth_data %>% 
+              group_by(Date) %>% 
+              summarise(Total_confirmed=sum(`Total Malaria Confirmed and Clinical`)),
+            by = join_by(Date)) %>%
+  ggplot(aes(x=Date, y=Total_confirmed)) +
+  geom_line() +
+  labs(y="Total number of clinically confirmed malaria cases") +
+  theme_light()
+
+# spatial pattern
+eth_map %>% 
+  group_by(ADM2_EN) %>%
+  summarise(geometry=st_union(geometry)) %>%
+  ungroup() %>%
+  rename(ZoneName=ADM2_EN) %>%
+  left_join(eth_data %>% 
+              group_by(ZoneName) %>%
+              summarise(Total_confirmed=sum(`Total Malaria Confirmed and Clinical`)),
+            by=join_by(ZoneName)) %>%
+  ggplot(aes(fill=Total_confirmed)) + 
+  geom_sf() +
+  scale_fill_distiller(name="confirmed", palette = "Reds", direction=1)+
+  theme_light()
+
--- Ethiopia/codes/eth_modelling.R
+++ Ethiopia/codes/eth_modelling.R
@@ -3,30 +3,32 @@ library("sf")
 library("tidyverse")
 
 # =========================================================
-# path to the malaria data file
-data_path <- "~/Downloads/Ethiopia/"
 
-# reading the malaria data
-eth_data <- readRDS(paste0(data_path, "eth_data.rds"))
+# read the EPHI weekly malaria surveillance data
+eth_data <- readRDS("~/Downloads/Ethiopia/eth_data.rds")
 
-# reading spatial covariates 
+# read spatial covariates 
 spat_covars <- 
   readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/spat_covars.rds"))
 
-# reading spatiotemporal covariates
+spat_covars <- spat_covars %>%
+  mutate(land_cover_percent=as.numeric(land_cover_percent))
+
+# read spatiotemporal covariates
 spat_temp_covars <- 
   readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/spat_temp_covars.rds"))
 
-# join all covariates
+# merge all covariates
 spat_temp_covars <- spat_temp_covars %>% 
   left_join(spat_covars, by=join_by(ADM2_EN)) %>%
   rename(ZoneName=ADM2_EN,
          Year=epi_year, 
          Epidemic_Week=epi_week)
 
-# data from a shapefile provided by OCHA
+# data administrative boundary data from a shapefile provided by OCHA
 eth_map <- 
   readRDS(url("https://github.com/jalilian/CEASE/raw/main/Ethiopia/ETH_Admin_2021_OCHA.rds"))
+
 # merge Woreds in each zone
 eth_map <- eth_map %>%
   group_by(ADM1_EN, ADM2_EN) %>%
@@ -34,6 +36,7 @@ eth_map <- eth_map %>%
             Total_pop=sum(Total)) %>%
   ungroup() %>%
   rename(RegionName=ADM1_EN, ZoneName=ADM2_EN)
+
 # =========================================================
 
 # remove all records of week 53 of the year 2015
@@ -41,24 +44,34 @@ eth_data <- eth_data %>%
   filter(!is.na(Date))
 
 eth_data <- eth_data %>% 
+  # create time index
   left_join(tibble(Date=seq(as.Date("2013-01-07"), 
                             as.Date("2022-08-15"), 
                             by=7)) %>%
-              mutate(idx_time=1:length(Date)),
+              mutate(idx_time=1:length(Date),
+                     idx_time2=idx_time),
             by=join_by(Date)) %>%
+  # create zone index
   left_join(eth_map %>% 
-              mutate(idx_zone=1:length(ZoneName)) %>%
+              mutate(idx_region=match(RegionName, unique(RegionName)),
+                     idx_zone=1:length(ZoneName)) %>%
               as_tibble() %>%
-              select(RegionName, ZoneName, idx_zone, Total_pop),
-            by=join_by(RegionName, ZoneName))
-
-eth_data <- eth_data %>%
-  rename(Total_confirmed=`Total Malaria Confirmed and Clinical`)
-
-eth_data <- eth_data %>%
+              select(RegionName, ZoneName, 
+                     idx_region, idx_zone, 
+                     Total_pop),
+            by=join_by(RegionName, ZoneName)) %>%
+  # rename the reponse variable
+  rename(Total_confirmed=`Total Malaria Confirmed and Clinical`) %>%
+  # merge covariates
   left_join(spat_temp_covars, 
             by=join_by(ZoneName, Year, Epidemic_Week))
+
+# comute the expected number of cases under the null model
+# of spatiotemporal homogeneity
+eth_data <- eth_data %>%
+  mutate(E=Total_pop * sum(Total_confirmed) / sum(Total_pop))
 # =========================================================
+
 library("INLA")
 
 ###
@@ -99,7 +112,8 @@ trendplot <- function(fit)
     xlab('date') + ylab('RW2 temporal random effect') +
     #labs(title = NULL, subtitle = NULL, caption = NULL) + 
     scale_x_date(date_breaks = "12 month" , date_labels = "%b-%Y") + 
-    theme(axis.text.x = element_text(angle = 0, hjust = 1))
+    theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
+    theme_light()
 }
 
 seasplot <- function(fit)
@@ -110,7 +124,8 @@ seasplot <- function(fit)
          mapping=aes(x=unique(fit$.args$data$Epidemic_Week), y=mean)) +
     geom_line() + 
     geom_ribbon(aes(ymin=q0.025, ymax=q0.975), alpha=0.25, col='blue') +
-    xlab('date') + ylab('RW1 temporal random effect')
+    xlab('date') + ylab('RW2 temporal random effect') +
+    theme_light()
 }
 
 
@@ -124,9 +139,8 @@ spatialplot <- function(fit, cmap=eth_map)
   ff <- cmap %>% left_join(dat, by=join_by(ZoneName))
   ggplot(data=ff, aes(fill=mean)) +
     geom_sf()+
-    scale_fill_distiller(palette = "Reds", direction=1)
-  
-
+    scale_fill_distiller(palette = "Reds", direction=1) +
+    theme_light()
 }
 
 library("spdep")
@@ -136,34 +150,41 @@ nb2INLA("map.graph", tmp)
 H <- inla.read.graph(filename="map.graph")
 image(inla.graph2matrix(H), xlab="", ylab="")
 
-fit <- inla(Total_confirmed ~ 
-              u10_mean + u10_min + u10_max + u10_sd +
-              v10_mean + v10_min + v10_max + v10_sd + 
-              lai_hv_mean + lai_hv_min + lai_hv_max+ lai_hv_sd + 
-              lai_lv_mean + lai_lv_min + lai_lv_max + lai_lv_sd +
-              skt_mean + skt_min + skt_max + skt_sd +
-              #sp_mean + sp_min + sp_max + sp_sd,# +
-              tp_mean + tp_min + tp_max + tp_sd + 
-              swvl1_mean + swvl1_min + swvl1_max + swvl1_sd +
-              pop_density_mean + pop_density_sd + 
-              land_cover_mode + land_cover_percent +
-              elevation_mean + elevation_sd +
-              f(idx_time, model="rw2") +
-              f(Epidemic_Week, model="rw2", cyclic=TRUE) +
-              f(idx_zone, model='bym2', graph=H, 
-                scale.model=TRUE, constr=TRUE, 
-                adjust.for.con.comp=TRUE),
-              data=eth_data, family = "nbinomial",
-            control.predictor=list(compute=TRUE, link=1),
-            control.compute=list(config=FALSE, waic=TRUE, dic=TRUE, cpo=TRUE),
-            num.threads=4, verbose=TRUE, keep=TRUE)
-
+fit <- 
+  inla(Total_confirmed ~ 
+         u10_mean + u10_min + u10_max + u10_sd +
+         v10_mean + v10_min + v10_max + v10_sd + 
+         lai_hv_mean + lai_hv_min + lai_hv_max+ lai_hv_sd + 
+         lai_lv_mean + lai_lv_min + lai_lv_max + lai_lv_sd +
+         skt_mean + skt_min + skt_max + skt_sd +
+         #sp_mean + sp_min + sp_max + sp_sd,# +
+         tp_mean + tp_min + tp_max + tp_sd + 
+         swvl1_mean + swvl1_min + swvl1_max + swvl1_sd +
+         pop_density_mean + pop_density_sd + 
+         land_cover_mode + land_cover_percent +
+         elevation_mean + elevation_sd +
+         f(idx_time, model="rw2") +
+         f(Epidemic_Week, model="rw2", #group=idx_zone, 
+           cyclic=TRUE) +
+         f(idx_time2, model="ar1") + 
+         f(idx_region, model="iid") + 
+         f(idx_zone, model='bym2', graph=H, 
+           scale.model=TRUE, constr=TRUE, 
+           adjust.for.con.comp=TRUE),
+       data=eth_data, E=E,
+       family = "nbinomial",
+       control.predictor=list(compute=TRUE, link=1),
+       control.compute=list(config=FALSE, waic=TRUE, dic=TRUE, cpo=TRUE),
+       num.threads=4, verbose=TRUE, keep=TRUE)
 
 printmodel(fit)
 pitfun(fit)
+
+fit$summary.hyperpar
 
 trendplot(fit)
 seasplot(fit)
 spatialplot(fit)
 
-fit$summary.fixed
+fit$summary.fixed %>%
+  filter(`0.025quant` * `0.975quant` > 0)
